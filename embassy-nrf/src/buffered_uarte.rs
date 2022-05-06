KerryRJ/embassy

Replace embassy::io with embedded_io.

embassy-nrf/src/buffered_uarte.rs
@@ -14,18 +14,17 @@
 //! Please also see [crate::uarte] to understand when [BufferedUarte] should be used.
 
 use core::cmp::min;
+use core::future::Future;
 use core::marker::PhantomData;
-use core::mem;
-use core::pin::Pin;
 use core::sync::atomic::{compiler_fence, Ordering};
-use core::task::{Context, Poll};
+use core::task::Poll;
 use embassy::interrupt::InterruptExt;
-use embassy::io::{AsyncBufRead, AsyncWrite};
 use embassy::util::Unborrow;
 use embassy::waitqueue::WakerRegistration;
 use embassy_hal_common::peripheral::{PeripheralMutex, PeripheralState, StateStorage};
 use embassy_hal_common::ring_buffer::RingBuffer;
 use embassy_hal_common::{low_power_wait_until, unborrow};
+use futures::future::poll_fn;
 
 use crate::gpio::Pin as GpioPin;
 use crate::pac;
@@ -197,82 +196,99 @@ impl<'d, U: UarteInstance, T: TimerInstance> BufferedUarte<'d, U, T> {
     }
 }
 
-impl<'d, U: UarteInstance, T: TimerInstance> AsyncBufRead for BufferedUarte<'d, U, T> {
-    fn poll_fill_buf(
-        mut self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-    ) -> Poll<embassy::io::Result<&[u8]>> {
-        self.inner.with(|state| {
-            compiler_fence(Ordering::SeqCst);
-            trace!("poll_read");
+impl<'d, U: UarteInstance, T: TimerInstance> embedded_io::Io for BufferedUarte<'d, U, T> {
+    type Error = core::convert::Infallible;
+}
 
-            // We have data ready in buffer? Return it.
-            let buf = state.rx.pop_buf();
-            if !buf.is_empty() {
-                trace!("  got {:?} {:?}", buf.as_ptr() as u32, buf.len());
-                let buf: &[u8] = buf;
-                let buf: &[u8] = unsafe { mem::transmute(buf) };
-                return Poll::Ready(Ok(buf));
+impl<'d, U: UarteInstance, T: TimerInstance> embedded_io::async_::Read for BufferedUarte<'d, U, T> {
+    type ReadFuture<'a> = impl Future<Output = Result<usize, Self::Error>>
+    where
+        Self: 'a;
+
+    fn read<'a>(&'a mut self, buf: &'a mut [u8]) -> Self::ReadFuture<'a> {
+        poll_fn(move |cx| {
+            let mut do_pend = false;
+            let res = self.inner.with(|state| {
+                compiler_fence(Ordering::SeqCst);
+                trace!("poll_read");
+
+                // We have data ready in buffer? Return it.
+                let data = state.rx.pop_buf();
+                if !data.is_empty() {
+                    trace!("  got {:?} {:?}", data.as_ptr() as u32, data.len());
+                    let len = data.len().min(data.len());
+                    buf[..len].copy_from_slice(&data[..len]);
+                    state.rx.pop(len);
+                    do_pend = true;
+                    return Poll::Ready(Ok(len));
+                }
+
+                trace!("  empty");
+                state.rx_waker.register(cx.waker());
+                Poll::Pending
+            });
+            if do_pend {
+                self.inner.pend();
             }
 
-            trace!("  empty");
-            state.rx_waker.register(cx.waker());
-            Poll::<embassy::io::Result<&[u8]>>::Pending
+            res
         })
-    }
-
-    fn consume(mut self: Pin<&mut Self>, amt: usize) {
-        self.inner.with(|state| {
-            trace!("consume {:?}", amt);
-            state.rx.pop(amt);
-        });
-        self.inner.pend();
     }
 }
 
-impl<'d, U: UarteInstance, T: TimerInstance> AsyncWrite for BufferedUarte<'d, U, T> {
-    fn poll_write(
-        mut self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-        buf: &[u8],
-    ) -> Poll<embassy::io::Result<usize>> {
-        let poll = self.inner.with(|state| {
-            trace!("poll_write: {:?}", buf.len());
+impl<'d, U: UarteInstance, T: TimerInstance> embedded_io::async_::Write
+    for BufferedUarte<'d, U, T>
+{
+    type WriteFuture<'a> = impl Future<Output = Result<usize, Self::Error>>
+    where
+        Self: 'a;
 
-            let tx_buf = state.tx.push_buf();
-            if tx_buf.is_empty() {
-                trace!("poll_write: pending");
-                state.tx_waker.register(cx.waker());
-                return Poll::Pending;
-            }
+    fn write<'a>(&'a mut self, buf: &'a [u8]) -> Self::WriteFuture<'a> {
+        poll_fn(move |cx| {
+            let res = self.inner.with(|state| {
+                trace!("poll_write: {:?}", buf.len());
 
-            let n = min(tx_buf.len(), buf.len());
-            tx_buf[..n].copy_from_slice(&buf[..n]);
-            state.tx.push(n);
+                let tx_buf = state.tx.push_buf();
+                if tx_buf.is_empty() {
+                    trace!("poll_write: pending");
+                    state.tx_waker.register(cx.waker());
+                    return Poll::Pending;
+                }
 
-            trace!("poll_write: queued {:?}", n);
+                let n = min(tx_buf.len(), buf.len());
+                tx_buf[..n].copy_from_slice(&buf[..n]);
+                state.tx.push(n);
 
-            compiler_fence(Ordering::SeqCst);
+                trace!("poll_write: queued {:?}", n);
 
-            Poll::Ready(Ok(n))
-        });
+                compiler_fence(Ordering::SeqCst);
 
-        self.inner.pend();
+                Poll::Ready(Ok(n))
+            });
 
-        poll
+            self.inner.pend();
+
+            res
+        })
     }
 
-    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<embassy::io::Result<()>> {
-        self.inner.with(|state| {
-            trace!("poll_flush");
+    type FlushFuture<'a> = impl Future<Output = Result<(), Self::Error>>
+    where
+        Self: 'a;
 
-            if !state.tx.is_empty() {
-                trace!("poll_flush: pending");
-                state.tx_waker.register(cx.waker());
-                return Poll::Pending;
-            }
+    fn flush<'a>(&'a mut self) -> Self::FlushFuture<'a> {
+        poll_fn(move |cx| {
+            self.inner.with(|state| {
+                trace!("poll_flush");
 
-            Poll::Ready(Ok(()))
+                if !state.tx.is_empty() {
+                    trace!("poll_flush: pending");
+                    state.tx_waker.register(cx.waker());
+                    return Poll::Pending;
+                }
+
+                Poll::Ready(Ok(()))
+            })
         })
     }
 }

examples/nrf/src/bin/buffered_uart.rs
@@ -4,9 +4,9 @@
 
 use defmt::*;
 use embassy::executor::Spawner;
-use embassy::io::{AsyncBufReadExt, AsyncWriteExt};
 use embassy_nrf::buffered_uarte::State;
 use embassy_nrf::{buffered_uarte::BufferedUarte, interrupt, uarte, Peripherals};
+use embedded_io::async_::{Read, Write};
 use futures::pin_mut;
 
 use defmt_rtt as _; // global logger

examples/std/src/bin/net.rs
@@ -2,12 +2,13 @@
 
 use clap::Parser;
 use embassy::executor::{Executor, Spawner};
-use embassy::io::AsyncWriteExt;
 use embassy::util::Forever;
+use embassy_net::tcp::TcpSocket;
 use embassy_net::{
     Config, Configurator, DhcpConfigurator, Ipv4Address, Ipv4Cidr, StackResources,
-    StaticConfigurator, TcpSocket,
+    StaticConfigurator,
 };
+use embedded_io::async_::Write;
 use heapless::Vec;
 use log::*;
 

examples/std/src/bin/serial.rs
@@ -5,8 +5,8 @@ mod serial_port;
 
 use async_io::Async;
 use embassy::executor::Executor;
-use embassy::io::AsyncBufReadExt;
 use embassy::util::Forever;
+use embedded_io::async_::Read;
 use log::*;
 use nix::sys::termios;
 
@@ -24,12 +24,12 @@ async fn run() {
     // Essentially, async_io::Async converts from AsRawFd+Read+Write to futures's AsyncRead+AsyncWrite
     let port = Async::new(port).unwrap();
 
-    // This implements futures's AsyncBufRead based on futures's AsyncRead
-    let port = futures::io::BufReader::new(port);
-
-    // We can then use FromStdIo to convert from futures's AsyncBufRead+AsyncWrite
-    // to embassy's AsyncBufRead+AsyncWrite
-    let mut port = embassy::io::FromStdIo::new(port);
+    // We can then use FromStdIo to convert from futures's AsyncRead+AsyncWrite
+    // to embedded_io's async Read+Write.
+    //
+    // This is not really needed, you could write the code below using futures::io directly.
+    // It's useful if you want to have portable code across embedded and std.
+    let mut port = embedded_io::adapters::FromFutures::new(port);
 
     info!("Serial opened!");
 

examples/stm32f7/src/bin/eth.rs
@@ -5,12 +5,10 @@
 use cortex_m_rt::entry;
 use defmt::*;
 use embassy::executor::{Executor, Spawner};
-use embassy::io::AsyncWriteExt;
 use embassy::time::{Duration, Timer};
 use embassy::util::Forever;
-use embassy_net::{
-    Config as NetConfig, Ipv4Address, Ipv4Cidr, StackResources, StaticConfigurator, TcpSocket,
-};
+use embassy_net::tcp::TcpSocket;
+use embassy_net::{Config as NetConfig, Ipv4Address, Ipv4Cidr, StackResources, StaticConfigurator};
 use embassy_stm32::eth::generic_smi::GenericSMI;
 use embassy_stm32::eth::{Ethernet, State};
 use embassy_stm32::interrupt;
@@ -19,6 +17,7 @@ use embassy_stm32::peripherals::RNG;
 use embassy_stm32::rng::Rng;
 use embassy_stm32::time::U32Ext;
 use embassy_stm32::Config;
+use embedded_io::async_::Write;
 use heapless::Vec;
 
 use defmt_rtt as _; // global logger

examples/stm32h7/src/bin/eth.rs
@@ -8,12 +8,10 @@ use panic_probe as _;
 use cortex_m_rt::entry;
 use defmt::*;
 use embassy::executor::{Executor, Spawner};
-use embassy::io::AsyncWriteExt;
 use embassy::time::{Duration, Timer};
 use embassy::util::Forever;
-use embassy_net::{
-    Config as NetConfig, Ipv4Address, Ipv4Cidr, StackResources, StaticConfigurator, TcpSocket,
-};
+use embassy_net::tcp::TcpSocket;
+use embassy_net::{Config as NetConfig, Ipv4Address, Ipv4Cidr, StackResources, StaticConfigurator};
 use embassy_stm32::eth::generic_smi::GenericSMI;
 use embassy_stm32::eth::{Ethernet, State};
 use embassy_stm32::interrupt;
@@ -22,6 +20,7 @@ use embassy_stm32::peripherals::RNG;
 use embassy_stm32::rng::Rng;
 use embassy_stm32::time::U32Ext;
 use embassy_stm32::Config;
+use embedded_io::async_::Write;
 use heapless::Vec;
 
 #[embassy::task]

examples/stm32l0/src/bin/usart_irq.rs
@@ -2,13 +2,14 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-use defmt_rtt as _; // global logger
+use defmt_rtt as _;
+use embedded_io::async_::{Read, Write};
+// global logger
 use panic_probe as _;
 
 use defmt::*;
 
 use embassy::executor::Spawner;
-use embassy::io::{AsyncBufReadExt, AsyncWriteExt};
 use embassy_stm32::dma::NoDma;
 use embassy_stm32::interrupt;
 use embassy_stm32::usart::{BufferedUart, Config, State, Uart};
@@ -16,19 +17,21 @@ use embassy_stm32::Peripherals;
 
 #[embassy::main]
 async fn main(_spawner: Spawner, p: Peripherals) {
+    info!("Hi!");
+
     static mut TX_BUFFER: [u8; 8] = [0; 8];
     static mut RX_BUFFER: [u8; 256] = [0; 256];
 
     let mut config = Config::default();
     config.baudrate = 9600;
 
-    let usart = Uart::new(p.USART1, p.PA10, p.PA9, NoDma, NoDma, config);
+    let usart = Uart::new(p.USART2, p.PA3, p.PA2, NoDma, NoDma, config);
     let mut state = State::new();
     let mut usart = unsafe {
         BufferedUart::new(
             &mut state,
             usart,
-            interrupt::take!(USART1),
+            interrupt::take!(USART2),
             &mut TX_BUFFER,
             &mut RX_BUFFER,
         )

KMSorSMS/embassy_preempt

embassy-nrf/src/buffered_uarte.rs
@@ -14,18 +14,17 @@
 //! Please also see [crate::uarte] to understand when [BufferedUarte] should be used.
 
 use core::cmp::min;
+use core::future::Future;
 use core::marker::PhantomData;
-use core::mem;
-use core::pin::Pin;
 use core::sync::atomic::{compiler_fence, Ordering};
-use core::task::{Context, Poll};
+use core::task::Poll;
 use embassy::interrupt::InterruptExt;
-use embassy::io::{AsyncBufRead, AsyncWrite};
 use embassy::util::Unborrow;
 use embassy::waitqueue::WakerRegistration;
 use embassy_hal_common::peripheral::{PeripheralMutex, PeripheralState, StateStorage};
 use embassy_hal_common::ring_buffer::RingBuffer;
 use embassy_hal_common::{low_power_wait_until, unborrow};
+use futures::future::poll_fn;
 
 use crate::gpio::Pin as GpioPin;
 use crate::pac;
@@ -197,82 +196,99 @@ impl<'d, U: UarteInstance, T: TimerInstance> BufferedUarte<'d, U, T> {
     }
 }
 
-impl<'d, U: UarteInstance, T: TimerInstance> AsyncBufRead for BufferedUarte<'d, U, T> {
-    fn poll_fill_buf(
-        mut self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-    ) -> Poll<embassy::io::Result<&[u8]>> {
-        self.inner.with(|state| {
-            compiler_fence(Ordering::SeqCst);
-            trace!("poll_read");
+impl<'d, U: UarteInstance, T: TimerInstance> embedded_io::Io for BufferedUarte<'d, U, T> {
+    type Error = core::convert::Infallible;
+}
 
-            // We have data ready in buffer? Return it.
-            let buf = state.rx.pop_buf();
-            if !buf.is_empty() {
-                trace!("  got {:?} {:?}", buf.as_ptr() as u32, buf.len());
-                let buf: &[u8] = buf;
-                let buf: &[u8] = unsafe { mem::transmute(buf) };
-                return Poll::Ready(Ok(buf));
+impl<'d, U: UarteInstance, T: TimerInstance> embedded_io::asynch::Read for BufferedUarte<'d, U, T> {
+    type ReadFuture<'a> = impl Future<Output = Result<usize, Self::Error>>
+    where
+        Self: 'a;
+
+    fn read<'a>(&'a mut self, buf: &'a mut [u8]) -> Self::ReadFuture<'a> {
+        poll_fn(move |cx| {
+            let mut do_pend = false;
+            let res = self.inner.with(|state| {
+                compiler_fence(Ordering::SeqCst);
+                trace!("poll_read");
+
+                // We have data ready in buffer? Return it.
+                let data = state.rx.pop_buf();
+                if !data.is_empty() {
+                    trace!("  got {:?} {:?}", data.as_ptr() as u32, data.len());
+                    let len = data.len().min(data.len());
+                    buf[..len].copy_from_slice(&data[..len]);
+                    state.rx.pop(len);
+                    do_pend = true;
+                    return Poll::Ready(Ok(len));
+                }
+
+                trace!("  empty");
+                state.rx_waker.register(cx.waker());
+                Poll::Pending
+            });
+            if do_pend {
+                self.inner.pend();
             }
 
-            trace!("  empty");
-            state.rx_waker.register(cx.waker());
-            Poll::<embassy::io::Result<&[u8]>>::Pending
+            res
         })
-    }
-
-    fn consume(mut self: Pin<&mut Self>, amt: usize) {
-        self.inner.with(|state| {
-            trace!("consume {:?}", amt);
-            state.rx.pop(amt);
-        });
-        self.inner.pend();
     }
 }
 
-impl<'d, U: UarteInstance, T: TimerInstance> AsyncWrite for BufferedUarte<'d, U, T> {
-    fn poll_write(
-        mut self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-        buf: &[u8],
-    ) -> Poll<embassy::io::Result<usize>> {
-        let poll = self.inner.with(|state| {
-            trace!("poll_write: {:?}", buf.len());
+impl<'d, U: UarteInstance, T: TimerInstance> embedded_io::asynch::Write
+    for BufferedUarte<'d, U, T>
+{
+    type WriteFuture<'a> = impl Future<Output = Result<usize, Self::Error>>
+    where
+        Self: 'a;
 
-            let tx_buf = state.tx.push_buf();
-            if tx_buf.is_empty() {
-                trace!("poll_write: pending");
-                state.tx_waker.register(cx.waker());
-                return Poll::Pending;
-            }
+    fn write<'a>(&'a mut self, buf: &'a [u8]) -> Self::WriteFuture<'a> {
+        poll_fn(move |cx| {
+            let res = self.inner.with(|state| {
+                trace!("poll_write: {:?}", buf.len());
 
-            let n = min(tx_buf.len(), buf.len());
-            tx_buf[..n].copy_from_slice(&buf[..n]);
-            state.tx.push(n);
+                let tx_buf = state.tx.push_buf();
+                if tx_buf.is_empty() {
+                    trace!("poll_write: pending");
+                    state.tx_waker.register(cx.waker());
+                    return Poll::Pending;
+                }
 
-            trace!("poll_write: queued {:?}", n);
+                let n = min(tx_buf.len(), buf.len());
+                tx_buf[..n].copy_from_slice(&buf[..n]);
+                state.tx.push(n);
 
-            compiler_fence(Ordering::SeqCst);
+                trace!("poll_write: queued {:?}", n);
 
-            Poll::Ready(Ok(n))
-        });
+                compiler_fence(Ordering::SeqCst);
 
-        self.inner.pend();
+                Poll::Ready(Ok(n))
+            });
 
-        poll
+            self.inner.pend();
+
+            res
+        })
     }
 
-    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<embassy::io::Result<()>> {
-        self.inner.with(|state| {
-            trace!("poll_flush");
+    type FlushFuture<'a> = impl Future<Output = Result<(), Self::Error>>
+    where
+        Self: 'a;
 
-            if !state.tx.is_empty() {
-                trace!("poll_flush: pending");
-                state.tx_waker.register(cx.waker());
-                return Poll::Pending;
-            }
+    fn flush<'a>(&'a mut self) -> Self::FlushFuture<'a> {
+        poll_fn(move |cx| {
+            self.inner.with(|state| {
+                trace!("poll_flush");
 
-            Poll::Ready(Ok(()))
+                if !state.tx.is_empty() {
+                    trace!("poll_flush: pending");
+                    state.tx_waker.register(cx.waker());
+                    return Poll::Pending;
+                }
+
+                Poll::Ready(Ok(()))
+            })
         })
     }
 }

examples/nrf/src/bin/buffered_uart.rs
@@ -4,9 +4,9 @@
 
 use defmt::*;
 use embassy::executor::Spawner;
-use embassy::io::{AsyncBufReadExt, AsyncWriteExt};
 use embassy_nrf::buffered_uarte::State;
 use embassy_nrf::{buffered_uarte::BufferedUarte, interrupt, uarte, Peripherals};
+use embedded_io::asynch::{Read, Write};
 use futures::pin_mut;
 
 use defmt_rtt as _; // global logger

examples/std/src/bin/net.rs
@@ -2,12 +2,13 @@
 
 use clap::Parser;
 use embassy::executor::{Executor, Spawner};
-use embassy::io::AsyncWriteExt;
 use embassy::util::Forever;
+use embassy_net::tcp::TcpSocket;
 use embassy_net::{
     Config, Configurator, DhcpConfigurator, Ipv4Address, Ipv4Cidr, StackResources,
-    StaticConfigurator, TcpSocket,
+    StaticConfigurator,
 };
+use embedded_io::asynch::Write;
 use heapless::Vec;
 use log::*;
 

examples/std/src/bin/serial.rs
@@ -5,8 +5,8 @@ mod serial_port;
 
 use async_io::Async;
 use embassy::executor::Executor;
-use embassy::io::AsyncBufReadExt;
 use embassy::util::Forever;
+use embedded_io::asynch::Read;
 use log::*;
 use nix::sys::termios;
 
@@ -24,12 +24,12 @@ async fn run() {
     // Essentially, async_io::Async converts from AsRawFd+Read+Write to futures's AsyncRead+AsyncWrite
     let port = Async::new(port).unwrap();
 
-    // This implements futures's AsyncBufRead based on futures's AsyncRead
-    let port = futures::io::BufReader::new(port);
-
-    // We can then use FromStdIo to convert from futures's AsyncBufRead+AsyncWrite
-    // to embassy's AsyncBufRead+AsyncWrite
-    let mut port = embassy::io::FromStdIo::new(port);
+    // We can then use FromStdIo to convert from futures's AsyncRead+AsyncWrite
+    // to embedded_io's async Read+Write.
+    //
+    // This is not really needed, you could write the code below using futures::io directly.
+    // It's useful if you want to have portable code across embedded and std.
+    let mut port = embedded_io::adapters::FromFutures::new(port);
 
     info!("Serial opened!");
 

examples/stm32f7/src/bin/eth.rs
@@ -5,12 +5,10 @@
 use cortex_m_rt::entry;
 use defmt::*;
 use embassy::executor::{Executor, Spawner};
-use embassy::io::AsyncWriteExt;
 use embassy::time::{Duration, Timer};
 use embassy::util::Forever;
-use embassy_net::{
-    Config as NetConfig, Ipv4Address, Ipv4Cidr, StackResources, StaticConfigurator, TcpSocket,
-};
+use embassy_net::tcp::TcpSocket;
+use embassy_net::{Config as NetConfig, Ipv4Address, Ipv4Cidr, StackResources, StaticConfigurator};
 use embassy_stm32::eth::generic_smi::GenericSMI;
 use embassy_stm32::eth::{Ethernet, State};
 use embassy_stm32::interrupt;
@@ -19,6 +17,7 @@ use embassy_stm32::peripherals::RNG;
 use embassy_stm32::rng::Rng;
 use embassy_stm32::time::U32Ext;
 use embassy_stm32::Config;
+use embedded_io::asynch::Write;
 use heapless::Vec;
 
 use defmt_rtt as _; // global logger

examples/stm32h7/src/bin/eth.rs
@@ -8,12 +8,10 @@ use panic_probe as _;
 use cortex_m_rt::entry;
 use defmt::*;
 use embassy::executor::{Executor, Spawner};
-use embassy::io::AsyncWriteExt;
 use embassy::time::{Duration, Timer};
 use embassy::util::Forever;
-use embassy_net::{
-    Config as NetConfig, Ipv4Address, Ipv4Cidr, StackResources, StaticConfigurator, TcpSocket,
-};
+use embassy_net::tcp::TcpSocket;
+use embassy_net::{Config as NetConfig, Ipv4Address, Ipv4Cidr, StackResources, StaticConfigurator};
 use embassy_stm32::eth::generic_smi::GenericSMI;
 use embassy_stm32::eth::{Ethernet, State};
 use embassy_stm32::interrupt;
@@ -22,6 +20,7 @@ use embassy_stm32::peripherals::RNG;
 use embassy_stm32::rng::Rng;
 use embassy_stm32::time::U32Ext;
 use embassy_stm32::Config;
+use embedded_io::asynch::Write;
 use heapless::Vec;
 
 #[embassy::task]

examples/stm32l0/src/bin/usart_irq.rs
@@ -2,13 +2,14 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-use defmt_rtt as _; // global logger
+use defmt_rtt as _;
+use embedded_io::asynch::{Read, Write};
+// global logger
 use panic_probe as _;
 
 use defmt::*;
 
 use embassy::executor::Spawner;
-use embassy::io::{AsyncBufReadExt, AsyncWriteExt};
 use embassy_stm32::dma::NoDma;
 use embassy_stm32::interrupt;
 use embassy_stm32::usart::{BufferedUart, Config, State, Uart};
@@ -16,19 +17,21 @@ use embassy_stm32::Peripherals;
 
 #[embassy::main]
 async fn main(_spawner: Spawner, p: Peripherals) {
+    info!("Hi!");
+
     static mut TX_BUFFER: [u8; 8] = [0; 8];
     static mut RX_BUFFER: [u8; 256] = [0; 256];
 
     let mut config = Config::default();
     config.baudrate = 9600;
 
-    let usart = Uart::new(p.USART1, p.PA10, p.PA9, NoDma, NoDma, config);
+    let usart = Uart::new(p.USART2, p.PA3, p.PA2, NoDma, NoDma, config);
     let mut state = State::new();
     let mut usart = unsafe {
         BufferedUart::new(
             &mut state,
             usart,
-            interrupt::take!(USART1),
+            interrupt::take!(USART2),
             &mut TX_BUFFER,
             &mut RX_BUFFER,
         )