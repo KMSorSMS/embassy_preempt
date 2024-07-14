5077ca9763ce5c679545b314b74a7d61a0006d02
snh <2640327187@qq.com>
2024-07-14 19:48:23 +0800

    fin part of defination and import lazy_static

---
+++ ucosii/build.rs
@@ -0,0 +1,33 @@
+//! handle environment variables to make conditional compilation with feature flags
+
+use std::env;
+
+fn main(){
+    // get the value of the environment variable "OS_MAX_MEM_PART"
+    let os_max_mem_part: i32 = env::var("OS_MAX_MEM_PART").unwrap_or("0".to_string()).parse() // 尝试将字符串解析为i32类型
+    .unwrap_or(0); // 如果解析失败，使用0作为默认值
+    // if os_max_mem_part is bigger than 0, then add the feature "OS_MAX_MEM_PART_EN" to the compilation
+    if os_max_mem_part > 0 {
+        println!("cargo:rustc-cfg=OS_MAX_MEM_PART_EN");
+    }
+    // about feature OS_EVENT_EN
+    let os_q_en:i32 = env::var("OS_MAX_MEM_PART").unwrap_or("0".to_string()).parse() // 尝试将字符串解析为i32类型
+    .unwrap_or(0); // 如果解析失败，使用0作为默认值
+    let os_max_qs:i32 = env::var("OS_MAX_MEM_PART").unwrap_or("0".to_string()).parse() // 尝试将字符串解析为i32类型
+    .unwrap_or(0); // 如果解析失败，使用0作为默认值
+    let os_mbox_en :i32 = env::var("OS_MAX_MEM_PART").unwrap_or("0".to_string()).parse() // 尝试将字符串解析为i32类型
+    .unwrap_or(0); // 如果解析失败，使用0作为默认值
+    let os_sem_en:i32 = env::var("OS_MAX_MEM_PART").unwrap_or("0".to_string()).parse() // 尝试将字符串解析为i32类型
+    .unwrap_or(0); // 如果解析失败，使用0作为默认值
+    let os_mutex_en :i32 = env::var("OS_MAX_MEM_PART").unwrap_or("0".to_string()).parse() // 尝试将字符串解析为i32类型
+    .unwrap_or(0); // 如果解析失败，使用0作为默认值
+    if (os_q_en==1&&os_max_qs==1)||os_mbox_en==1||os_sem_en==1||os_mutex_en==1{
+        println!("cargo:rustc-cfg=OS_EVENT_EN");
+    }
+    // about feature OS_EVENT_NAME_EN
+    let os_event_name_en:i32 = env::var("OS_MAX_MEM_PART").unwrap_or("0".to_string()).parse() // 尝试将字符串解析为i32类型
+    .unwrap_or(0); // 如果解析失败，使用0作为默认值
+    if os_event_name_en==1 {
+        println!("cargo:rustc-cfg=OS_EVENT_NAME_EN");
+    }
+}
---
+++ ucosii/src/os_mem.rs
@@ -0,0 +1,64 @@
+/*
+*********************************************************************************************************
+*                                              uC/OS-II
+*                                        The Real-Time Kernel
+*
+*                    Copyright 1992-2021 Silicon Laboratories Inc. www.silabs.com
+*
+*                                 SPDX-License-Identifier: APACHE-2.0
+*
+*               This software is subject to an open source license and is distributed by
+*                Silicon Laboratories Inc. pursuant to the terms of the Apache License,
+*                    Version 2.0 available at www.apache.org/licenses/LICENSE-2.0.
+*                                    rewrite by liam and noah
+*
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
+*
+*                                           MEMORY MANAGEMENT
+*
+* Filename : os_mem.rs
+* Version  : V0.0.1
+*********************************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************************
+*                                      CREATE A MEMORY PARTITION
+*
+* Description : Create a fixed-sized memory partition that will be managed by uC/OS-II.
+*
+* Arguments   : addr     is the starting address of the memory partition
+*
+*               nblks    is the number of memory blocks to create from the partition.
+*
+*               blksize  is the size (in bytes) of each block in the memory partition.
+*
+*               perr     is a pointer to a variable containing an error message which will be set by
+*                        this function to either:
+*
+*                        OS_ERR_NONE                     if the memory partition has been created correctly.
+*                        OS_ERR_ILLEGAL_CREATE_RUN_TIME  if you tried to create a memory partition after
+*                                                        safety critical operation started.
+*                        OS_ERR_MEM_INVALID_ADDR         if you are specifying an invalid address for the memory
+*                                                        storage of the partition or, the block does not align
+*                                                        on a pointer boundary
+*                        OS_ERR_MEM_INVALID_PART         no free partitions available
+*                        OS_ERR_MEM_INVALID_BLKS         user specified an invalid number of blocks (must be >= 2)
+*                        OS_ERR_MEM_INVALID_SIZE         user specified an invalid block size
+*                                                          - must be greater than the size of a pointer
+*                                                          - must be able to hold an integral number of pointers
+* Returns    : != (OS_MEM *)0  is the partition was created
+*              == (OS_MEM *)0  if the partition was not created because of invalid arguments or, no
+*                              free partition is available.
+*********************************************************************************************************
+*/
+#[allow(unused)]
+#[no_mangle]
+pub fn OSMemCreate(addr: *mut u8, nblks: u16, blksize: u16, perr: &mut OS_ERR) -> *mut OS_MEM {
+
+}
