660e430099af0adcde965e83fbb3d001230a04ca
liam <liamyzw@126.com>
2024-07-30 16:57:37 +0800

    fin executor's poll section

--- ucosii/src/heap/stack_allocator.rs
+++ ucosii/src/heap/stack_allocator.rs
@@ -23,12 +23,12 @@ pub fn init_stack_allocator() {
 }
 /// alloc a new stack
 pub fn alloc_stack(layout: Layout) -> OS_STK_REF {
-    unsafe { stk_from_ptr(ALLOCATOR.alloc(layout) as *mut OS_STK,layout) }
+    unsafe { stk_from_ptr(ALLOCATOR.alloc(layout), layout) }
 }
 /// dealloc a stack
 pub fn dealloc_stack(stk: OS_STK_REF) {
     unsafe {
-        ALLOCATOR.dealloc(stk.as_ptr() as *mut u8, stk.layout);
+        ALLOCATOR.dealloc(stk.as_ptr(), stk.layout);
     }
 }
 
@@ -36,6 +36,8 @@ pub fn dealloc_stack(stk: OS_STK_REF) {
 pub struct OS_STK_REF {
     /// the ref of the stk(top or bottom)
     pub STK_REF: NonNull<OS_STK>,
+    /// the ref of this dynamic stk's src heap
+    pub HEAP_REF: NonNull<u8>,
     /// the layout(size) of the stk
     pub layout: Layout,
 }
@@ -45,31 +47,30 @@ impl Default for OS_STK_REF {
     fn default() -> Self {
         OS_STK_REF {
             STK_REF: NonNull::dangling(),
-            layout:  Layout::from_size_align(0, 1).unwrap(),
+            HEAP_REF: NonNull::dangling(),
+            layout: Layout::from_size_align(0, 1).unwrap(),
         }
     }
 }
 /// we impl drop for OS_STK_REF to dealloc the stack(try to be RAII)
 impl Drop for OS_STK_REF {
     fn drop(&mut self) {
-        if self.STK_REF.as_ptr().is_null() {
-            return;
-        }
         unsafe {
-            ALLOCATOR.dealloc(self.STK_REF.as_ptr() as *mut u8, self.layout);
+            ALLOCATOR.dealloc(self.HEAP_REF.as_ptr(), self.layout);
         }
     }
 }
 
 impl OS_STK_REF {
-    pub fn as_ptr(&self) -> *mut OS_STK {
-        self.STK_REF.as_ptr()
+    pub fn as_ptr(&self) -> *mut u8 {
+        self.HEAP_REF.as_ptr()
     }
 }
 
-pub fn stk_from_ptr(stk_ptr: *mut OS_STK, layout: Layout) -> OS_STK_REF {
+pub fn stk_from_ptr(stk_ptr: *mut u8, layout: Layout) -> OS_STK_REF {
     OS_STK_REF {
-        STK_REF: NonNull::new(stk_ptr).unwrap(),
+        STK_REF: NonNull::new(unsafe { stk_ptr.offset(layout.size() as isize) as *mut OS_STK }).unwrap(),
+        HEAP_REF: NonNull::new(stk_ptr).unwrap(),
         layout,
     }
 }
