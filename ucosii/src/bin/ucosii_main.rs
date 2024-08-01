cce6e4b33af38a4e34f93392d01eb148264eb8c9
snh <2640327187@qq.com>
2024-08-01 16:00:30 +0800

    add test and define cs ourselves

--- ucosii/src/bin/ucosii_main.rs
+++ ucosii/src/bin/ucosii_main.rs
@@ -1,32 +1,73 @@
 #![no_main]
 #![no_std]
 #![feature(impl_trait_in_assoc_type)]
-use ucosii as _;
-use defmt::Format; // <- derive attribute
-#[derive(Format)]
-struct S1<T> {
-    x: u8,
-    y: T,
-}
+use core::arch::asm;
 
-#[derive(Format)]
-struct S2 {
-    z: u8,
-}
-fn hello() {
-    defmt::info!("Hello, world!");
-}
+use ucosii as _;
+use defmt::{info, Format}; // <- derive attribute
+use ucosii::{os_core::{OSInit, OSStart}, os_task::{OSTaskCreate, RustOSTaskCreate}};
+
+const LONG_TIME: usize = 10;
+const MID_TIME: usize = 5;
+const SHORT_TIME: usize = 3;
+
+// fn hello() {
+//     defmt::info!("Hello, world!");
+// }
 
 #[cortex_m_rt::entry]
-fn main() -> ! {
-    let s = S1 {
-        x: 42,
-        y: S2 { z: 43 },
-    };
-    hello();
-    defmt::println!("s={:?}", s);
-    let x = 42;
-    defmt::println!("x={=u8}", x);
+fn test_basic_schedule() -> ! {
+    // os初始化
+    OSInit();
+    // 创建两个任务
+    OSTaskCreate(task1, 0 as *mut (), 0 as *mut usize, 10);
+    OSTaskCreate(task2, 0 as *mut (), 0 as *mut usize, 11);
+    RustOSTaskCreate(task3, 0 as *mut (),0 as *mut usize, 12);
+    OSTaskCreate(task4, 0 as *mut (), 0 as *mut usize, 13);
+    // 启动os
+    OSStart();
+}
 
-    ucosii::lang_items::exit()
+fn task1(_args:*mut ()) {
+    // 任务1
+    info!("---task1 begin---");
+    delay(LONG_TIME);
+    info!("---task1 end---");
+    delay(SHORT_TIME);
+}
+fn task2(_args:*mut ()) {
+    // 任务2
+    info!("---task2 begin---");
+    delay(MID_TIME);
+    info!("---task2 end---");
+    delay(SHORT_TIME);
+}
+async fn task3(_args:*mut ()) {
+    // 任务3
+    loop{
+        //
+        info!("---task3 begin---");
+        delay(LONG_TIME);
+        info!("---task3 end---");
+        delay(SHORT_TIME);
+    }
+}
+fn task4(_args:*mut ()) {
+    // 任务4
+    info!("---task4 begin---");
+    // 任务3中涉及任务创建
+    OSTaskCreate(task1, 0 as *mut (), 0 as *mut usize, 14);
+    delay(SHORT_TIME);
+    info!("---task4 end---");
+    delay(SHORT_TIME);
+}
+fn delay(time: usize){
+    // 延时函数,time的单位约为0.5s
+    for _ in 0..time {
+        for _ in 0..200000/2 {
+            unsafe {
+                asm!("nop");
+            }
+        }
+    }
 }
--- ucosii/src/os_time.rs
+++ ucosii/src/os_time.rs
@@ -1,1 +1,96 @@
+use crate::port::INT32U;
+#[allow(unused)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+/// An Instant in time, based on the MCU's clock ticks since startup.
+pub struct Instant {
+    ticks: u64,
+}
 
+#[allow(unused)]
+/// A future that completes at a specified [Instant](struct.Instant.html).
+#[must_use = "futures do nothing unless you `.await` or poll them"]
+pub struct Timer {
+    expires_at: Instant,
+    yielded_once: bool,
+}
+
+// impl Timer {
+//     /// Expire at specified [Instant](struct.Instant.html)
+//     pub fn at(expires_at: Instant) -> Self {
+//         Self {
+//             expires_at,
+//             yielded_once: false,
+//         }
+//     }
+
+//     /// Expire after specified [Duration](struct.Duration.html).
+//     /// This can be used as a `sleep` abstraction.
+//     ///
+//     /// Example:
+//     /// ``` no_run
+//     /// use embassy_time::{Duration, Timer};
+//     ///
+//     /// #[embassy_executor::task]
+//     /// async fn demo_sleep_seconds() {
+//     ///     // suspend this task for one second.
+//     ///     Timer::after(Duration::from_secs(1)).await;
+//     /// }
+//     /// ```
+//     pub fn after(duration: Duration) -> Self {
+//         Self {
+//             expires_at: Instant::now() + duration,
+//             yielded_once: false,
+//         }
+//     }
+
+//     /// Expire after the specified number of ticks.
+//     ///
+//     /// This method is a convenience wrapper for calling `Timer::after(Duration::from_ticks())`.
+//     /// For more details, refer to [`Timer::after()`] and [`Duration::from_ticks()`].
+//     #[inline]
+//     pub fn after_ticks(ticks: u64) -> Self {
+//         Self::after(Duration::from_ticks(ticks))
+//     }
+
+//     /// Expire after the specified number of nanoseconds.
+//     ///
+//     /// This method is a convenience wrapper for calling `Timer::after(Duration::from_nanos())`.
+//     /// For more details, refer to [`Timer::after()`] and [`Duration::from_nanos()`].
+//     #[inline]
+//     pub fn after_nanos(nanos: u64) -> Self {
+//         Self::after(Duration::from_nanos(nanos))
+//     }
+
+//     /// Expire after the specified number of microseconds.
+//     ///
+//     /// This method is a convenience wrapper for calling `Timer::after(Duration::from_micros())`.
+//     /// For more details, refer to [`Timer::after()`] and [`Duration::from_micros()`].
+//     #[inline]
+//     pub fn after_micros(micros: u64) -> Self {
+//         Self::after(Duration::from_micros(micros))
+//     }
+
+//     /// Expire after the specified number of milliseconds.
+//     ///
+//     /// This method is a convenience wrapper for calling `Timer::after(Duration::from_millis())`.
+//     /// For more details, refer to [`Timer::after`] and [`Duration::from_millis()`].
+//     #[inline]
+//     pub fn after_millis(millis: u64) -> Self {
+//         Self::after(Duration::from_millis(millis))
+//     }
+
+//     /// Expire after the specified number of seconds.
+//     ///
+//     /// This method is a convenience wrapper for calling `Timer::after(Duration::from_secs())`.
+//     /// For more details, refer to [`Timer::after`] and [`Duration::from_secs()`].
+//     #[inline]
+//     pub fn after_secs(secs: u64) -> Self {
+//         Self::after(Duration::from_secs(secs))
+//     }
+// }
+
+/// an async delay
+pub async fn OSTimeDly(_ticks:INT32U){
+     
+}
