82b8302f82a40acc06f7173838c1de0367437fc2
snh <2640327187@qq.com>
2024-08-09 16:08:57 +0800

    push

--- ucosii/src/port/mod.rs
+++ ucosii/src/port/mod.rs
@@ -33,5 +33,35 @@ pub type USIZE = usize;
 pub type OS_STK = usize;
 /// Define size of CPU status register (PSR = 32 bits)
 pub type OS_CPU_SR = u32;
+/// the timer used as the time Driver
+#[cfg(feature="time_driver_tim1")]
+type T = peripherals::TIM1;
+#[cfg(feature="time_driver_tim2")]
+type T = peripherals::TIM2;
+#[cfg(feature="time_driver_tim3")]
+type T = peripherals::TIM3;
+#[cfg(feature="time_driver_tim4")]
+type T = peripherals::TIM4;
+#[cfg(feature="time_driver_tim5")]
+type T = peripherals::TIM5;
+#[cfg(feature="time_driver_tim8")]
+type T = peripherals::TIM8;
+#[cfg(feature="time_driver_tim9")]
+type T = peripherals::TIM9;
+#[cfg(feature="time_driver_tim12")]
+type T = peripherals::TIM12;
+#[cfg(feature="time_driver_tim15")]
+type T = peripherals::TIM15;
+#[cfg(feature="time_driver_tim20")]
+type T = peripherals::TIM20;
+#[cfg(feature="time_driver_tim21")]
+type T = peripherals::TIM21;
+#[cfg(feature="time_driver_tim22")]
+type T = peripherals::TIM22;
+#[cfg(feature="time_driver_tim23")]
+type T = peripherals::TIM23;
+#[cfg(feature="time_driver_tim24")]
+type T = peripherals::TIM24;
+
 
 pub mod os_cpu;
