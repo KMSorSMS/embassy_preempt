63d8b6003ea04198d003be708190ee03f1cfb361
liam <liamyzw@126.com>
2024-08-14 09:38:26 +0800

    complete timer queue

--- ucosii/src/executor/timer_queue.rs
+++ ucosii/src/executor/timer_queue.rs
@@ -1,6 +1,5 @@
-use crate::util::SyncUnsafeCell;
-
 use super::OS_TCB_REF;
+use crate::util::SyncUnsafeCell;
 
 pub(crate) struct TimerQueue {
     head: SyncUnsafeCell<Option<OS_TCB_REF>>,
@@ -13,17 +12,60 @@ impl TimerQueue {
         }
     }
     /// Insert a task into the timer queue.(sorted by `expires_at`,the header is the nearest task)
-    pub(crate) unsafe fn update(&self, p:OS_TCB_REF){
+    pub(crate) unsafe fn update(&self, p: OS_TCB_REF) {
         let head = self.head.get_unmut();
         let p_expires_at = &p.expires_at;
-        // range from head to find one smaller than p_expires_at and insert p.
-        let mut cur = head; 
+        if *p_expires_at.get_unmut() == u64::MAX {
+            return;
+        }
+        // range from head to find one larger than p_expires_at and insert p.
+        let mut cur = head;
         while let Some(cur_ref) = cur {
             let cur_expires_at = &cur_ref.expires_at;
             if cur_expires_at > p_expires_at {
                 break;
             }
             cur = cur_ref.OSTimerNext.get_unmut();
+        }
+        // insert p
+        p.OSTimerNext.set(*cur);
+        if let Some(cur_ref) = cur {
+            p.OSTimerPrev.set(*cur_ref.OSTimerPrev.get_unmut());
+            cur_ref.OSTimerPrev.set(Some(p));
+        }
+        if let Some(prev_ref) = p.OSTimerPrev.get_unmut() {
+            prev_ref.OSTimerNext.set(Some(p));
+        } else {
+            self.head.set(Some(p));
+        }
+    }
+    pub(crate) unsafe fn next_expiration(&self) -> u64 {
+        let head = self.head.get_unmut();
+        if let Some(head_ref) = head {
+            *head_ref.expires_at.get_unmut()
+        } else {
+            u64::MAX
+        }
+    }
+    pub(crate) unsafe fn dequeue_expired(&self, now: u64, on_task: impl Fn(OS_TCB_REF)) {
+        let head = self.head.get_unmut();
+        let mut cur = head;
+        while let Some(cur_ref) = cur {
+            let cur_expires_at = &cur_ref.expires_at;
+            if *cur_expires_at.get_unmut() > now {
+                break;
+            }
+            let next = cur_ref.OSTimerNext.get_unmut();
+            if let Some(next_ref) = next {
+                next_ref.OSTimerPrev.set(*cur_ref.OSTimerPrev.get_unmut());
+            }
+            if let Some(prev_ref) = cur_ref.OSTimerPrev.get_unmut() {
+                prev_ref.OSTimerNext.set(*next);
+            } else {
+                self.head.set(*next);
+            }
+            on_task(*cur_ref);
+            cur = next;
         }
     }
 }
