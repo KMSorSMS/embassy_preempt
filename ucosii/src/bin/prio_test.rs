36d01e85a022f8fcfa18708436cd9314426d0beb
snh <2640327187@qq.com>
2024-08-29 17:25:24 +0800

    merge

--- ucosii/src/bin/prio_test.rs
+++ ucosii/src/bin/prio_test.rs
@@ -25,7 +25,7 @@ fn main_test() -> ! {
 }
 fn test_basic_schedule() {
     #[cfg(feature = "defmt")]
-    trace!("==========test begin==========");
+    info!("==========test begin==========");
     // os初始化
     OSInit();
     // 创建6个任务，测试优先级调度的顺序是否正确
@@ -43,53 +43,53 @@ fn test_basic_schedule() {
 fn task1(_args: *mut c_void) {
     // 任务1
     #[cfg(feature = "defmt")]
-    trace!("---task1 begin---");
+    info!("---task1 begin---");
     delay(LONG_TIME);
     #[cfg(feature = "defmt")]
-    trace!("---task1 end---");
+    info!("---task1 end---");
     delay(SHORT_TIME);
 }
 fn task2(_args: *mut c_void) {
     // 任务2
     #[cfg(feature = "defmt")]
-    trace!("---task2 begin---");
+    info!("---task2 begin---");
     delay(MID_TIME);
     #[cfg(feature = "defmt")]
-    trace!("---task2 end---");
+    info!("---task2 end---");
     delay(SHORT_TIME);
 }
 async fn task3(_args: *mut c_void) {
     // 任务3
     //
     #[cfg(feature = "defmt")]
-    trace!("---task3 begin---");
+    info!("---task3 begin---");
     Timer::after_ticks(LONG_TIME as u64).await;
     // delay(LONG_TIME);
     #[cfg(feature = "defmt")]
-    trace!("---task3 end---");
+    info!("---task3 end---");
     delay(SHORT_TIME);
 }
 fn task4(_args: *mut c_void) {
     // 任务4
     #[cfg(feature = "defmt")]
-    trace!("---task4 begin---");
+    info!("---task4 begin---");
     // 任务4中涉及任务创建
     SyncOSTaskCreate(task1, 0 as *mut c_void, 0 as *mut usize, 34);
     delay(SHORT_TIME);
     #[cfg(feature = "defmt")]
-    trace!("---task4 end---");
+    info!("---task4 end---");
     delay(SHORT_TIME);
 }
 
 fn task5(_args: *mut c_void) {
     // 任务5
     #[cfg(feature = "defmt")]
-    trace!("---task5 begin---");
+    info!("---task5 begin---");
     // 任务5中涉及任务创建
     SyncOSTaskCreate(task1, 0 as *mut c_void, 0 as *mut usize, 11);
     delay(SHORT_TIME);
     #[cfg(feature = "defmt")]
-    trace!("---task5 end---");
+    info!("---task5 end---");
     delay(SHORT_TIME);
 }
 
@@ -97,11 +97,11 @@ fn task5(_args: *mut c_void) {
 fn task6(_args: *mut c_void) {
     // 任务6
     #[cfg(feature = "defmt")]
-    trace!("---task6 begin---");
+    info!("---task6 begin---");
     // 任务6中涉及任务创建，新创建的优先级与当前任务相同
     SyncOSTaskCreate(task1, 0 as *mut c_void, 0 as *mut usize, 35);
     delay(SHORT_TIME);
     #[cfg(feature = "defmt")]
-    trace!("---task6 end---");
+    info!("---task6 end---");
     delay(SHORT_TIME);
 }
--- ucosii/src/bin/scheduling1_test.rs
+++ ucosii/src/bin/scheduling1_test.rs
@@ -38,19 +38,19 @@ fn test_basic_schedule() {
 fn task1(_args: *mut c_void) {
     // 任务1
     #[cfg(feature = "defmt")]
-    trace!("---task1 begin---");
+    info!("---task1 begin---");
     delay(LONG_TIME);
     #[cfg(feature = "defmt")]
-    trace!("---task1 end---");
+    info!("---task1 end---");
     delay(SHORT_TIME);
 }
 fn task2(_args: *mut c_void) {
     // 任务2
     #[cfg(feature = "defmt")]
-    trace!("---task2 begin---");
+    info!("---task2 begin---");
     delay(MID_TIME);
     #[cfg(feature = "defmt")]
-    trace!("---task2 end---");
+    info!("---task2 end---");
     delay(SHORT_TIME);
 }
 async fn task3(_args: *mut c_void) {
@@ -58,23 +58,23 @@ async fn task3(_args: *mut c_void) {
     loop {
         //
         #[cfg(feature = "defmt")]
-        trace!("---task3 begin---");
+        info!("---task3 begin---");
         Timer::after_ticks(LONG_TIME as u64).await;
         // delay(LONG_TIME);
         Timer::after_millis(3000).await;
         #[cfg(feature = "defmt")]
-        trace!("---task3 end---");
+        info!("---task3 end---");
         delay(SHORT_TIME);
     }
 }
 fn task4(_args: *mut c_void) {
     // 任务4
     #[cfg(feature = "defmt")]
-    trace!("---task4 begin---");
+    info!("---task4 begin---");
     // 任务3中涉及任务创建
     SyncOSTaskCreate(task1, 0 as *mut c_void, 0 as *mut usize, 14);
     delay(SHORT_TIME);
     #[cfg(feature = "defmt")]
-    trace!("---task4 end---");
+    info!("---task4 end---");
     delay(MID_TIME);
 }
