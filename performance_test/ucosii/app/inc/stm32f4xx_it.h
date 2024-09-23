31c1a574deeed65e6889fdbc8066a7da091920ef
snh <2640327187@qq.com>
2024-09-23 11:42:14 +0800

    ser the performance test as submodule

--- performance_test/ucosii/app/inc/stm32f4xx_it.h
+++
@@ -1,6 +0,0 @@
-#ifndef __STM32F4xx_IT_H
-#define __STM32F4xx_IT_H
-
-void PendSV_Handler(void);
-
-#endif
--- performance_test/ucosii/app/src/stm32f4xx_it.c
+++
@@ -1,4 +0,0 @@
-#include "stm32f4xx_it.h"
-#include "ucos_ii.h"
-
-void SysTick_Handler(void) { OS_CPU_SysTickHandler(); }
